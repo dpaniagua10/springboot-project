d3cbd516c2fc92f95fb450f43d5749e0e88a0a9a
dpaniagua10 <dpaniagua2017@gmail.com>
2020-10-12 20:51:15 -0600

    second commit project

---
+++ src/main/java/net/springboot/java/SpringbootProjectWebApplication.java
@@ -0,0 +1,13 @@
+package net.springboot.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProjectWebApplication.class, args);
+	}
+
+}

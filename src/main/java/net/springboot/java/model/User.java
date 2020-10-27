07370604d3a4c03c87ebd3bb79dfc928e9c6948c
dpaniagua10 <dpaniagua2017@gmail.com>
2020-10-27 03:28:17 -0600

    reports and database postgresql

--- src/main/java/net/springboot/java/model/User.java
+++ src/main/java/net/springboot/java/model/User.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Collection;
 
 @Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Table(schema="public",uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
 
     @Id

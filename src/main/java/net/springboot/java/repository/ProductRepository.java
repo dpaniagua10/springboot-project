5e15d7bbd57111b923e84ec3595a8e93a33ea55f
dpaniagua10 <dpaniagua2017@gmail.com>
2020-10-28 01:34:28 -0600

    fixed

--- src/main/java/net/springboot/java/repository/ProductRepository.java
+++ src/main/java/net/springboot/java/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package net.springboot.java.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
 import net.springboot.java.model.Product;
 
+@Service
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 	
 	Product findFirstByCodigo(String codigo);
--- src/main/java/net/springboot/java/repository/ProductSoldRepository.java
+++ src/main/java/net/springboot/java/repository/ProductSoldRepository.java
@@ -1,9 +1,11 @@
 package net.springboot.java.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
 
 import net.springboot.java.model.ProductSold;
 
+@Service
 public interface ProductSoldRepository extends CrudRepository<ProductSold, Integer> {
 
 }
